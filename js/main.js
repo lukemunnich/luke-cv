94c23ac0f1132f057c4922ec86bdb9faa98def0c
luke munnich <lukemunnich@gmail.com>
2019-07-20 21:05:16 +0200

    added buttons and javascript functionality

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+  function mySecondiv() {
+    var x = document.getElementById("mySecondiv");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
